b0ca6e07c3b1cf77b685d193b54b8378e1e122e3
Rodrigo Kovalski <rodrigokvk@gmail.com>
2022-02-05 00:47:42 -0300

    Adicionando animação ao título

--- src/components/PageTitle/index.tsx
+++ src/components/PageTitle/index.tsx
@@ -1,19 +1,14 @@
 import styles from './styles.module.scss'
 
-import  BgGuy from '../../assets/desktop-guy.svg'
-
 export function PageTitle() {
     return (
         <div className={styles.wrapper}>
             <div className={styles.container}>
-                <div>
-                    <img src={BgGuy} alt="" />
-                </div>
                 <div className={styles.title}>
                     <h1>
                         Hey, I'm a <br />
-                            Front End Developer!
                     </h1>
+                    <p className={styles.typing}>Front End Developer!</p>
                 </div>
             </div>
         </div>

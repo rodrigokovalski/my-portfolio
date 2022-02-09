dbf55f0f55ac464ca044d83779499466ee34daf4
Rodrigo Kovalski <rodrigokvk@gmail.com>
2022-02-08 22:26:43 -0300

    udpate

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,13 +3,23 @@ import styles from './styles.module.scss'
 import { Link , animateScroll as scroll } from 'react-scroll'
 
 import ArrowUp from '../../assets/chevron-up.svg'
+import image from '../../assets/css.svg'
 
 export function Footer(){
     return (
         <footer className={styles.content} id="contact">
             <div className={styles.container}>
                 <div className={styles.contact}>
-                    
+                    <div className={styles.picture}>
+                    </div>
+                    <div className={styles.info}>
+                        <ul>
+                            <li>linkedin</li>
+                            <li>GitHub</li>
+                            <li>Instagram</li>
+                            <li>E-mail</li>
+                        </ul>
+                    </div>
                 </div>
                 <Link 
                     to="home"

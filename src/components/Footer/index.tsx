de8fb3e0e39ed4d15d0c63cf4128bc76252033cb
Rodrigo Kovalski <rodrigokvk@gmail.com>
2022-02-15 19:08:10 -0300

    update

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -16,28 +16,28 @@ export function Footer(){
             <div className={styles.container}>
                 <div className={styles.contact}>
                     <div className={styles.picture}>
-                        <img src={profilePicture} alt="" />
+                        <img src={profilePicture} alt="Rodrigo Kovaslski" />
                     </div>
                     <div className={styles.info}>
                         <ul>
                             <li>
                                 <a href="https://www.linkedin.com/in/rodrigokvk/" target="_blank">
-                                    <img src={Linkedin} alt="" /> <span>Linkedin/rodrigokvk</span>
+                                    <img src={Linkedin} alt="linkedin" /> <span>Linkedin/rodrigokvk</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="https://github.com/rodrigokovalski" target="_blank">
-                                    <img src={Github} alt="" /> <span>GitHub/rodrigokovalski</span>
+                                    <img src={Github} alt="github" /> <span>GitHub/rodrigokovalski</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="https://www.instagram.com/__rodrigkvii/" target="_blank">
-                                    <img src={Instagram} alt="" /> <span>@__rodrigkvii</span>
+                                    <img src={Instagram} alt="instagram" /> <span>@__rodrigkvii</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="mailto:rodrigokvk@gmail.com" target="_blank">
-                                    <img src={Email} alt="" /> <span>rodrigokvk@gmail.com</span>
+                                    <img src={Email} alt="gmail" /> <span>rodrigokvk@gmail.com</span>
                                 </a>
                             </li>
                         </ul>

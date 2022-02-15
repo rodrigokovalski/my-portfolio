ef25f8acb9f93a682dcd98100f504e2faf018560
Rodrigo Kovalski <rodrigokvk@gmail.com>
2022-02-15 19:26:59 -0300

    update

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,10 +4,6 @@ import { Link , animateScroll as scroll } from 'react-scroll'
 
 import ArrowUp from '../../assets/chevron-up.svg'
 
-import Instagram from '../../assets/instagram-square-brands.svg'
-import Linkedin from '../../assets/linkedin-brands.svg'
-import Github from '../../assets/github-square-brands.svg'
-import Email from '../../assets/envelope-solid.svg'
 
 export function Footer(){
     return (
@@ -18,26 +14,7 @@ export function Footer(){
                     </div>
                     <div className={styles.info}>
                         <ul>
-                            <li>
-                                <a href="https://www.linkedin.com/in/rodrigokvk/" target="_blank">
-                                    <img src={Linkedin} alt="linkedin" /> <span>Linkedin/rodrigokvk</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://github.com/rodrigokovalski" target="_blank">
-                                    <img src={Github} alt="github" /> <span>GitHub/rodrigokovalski</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://www.instagram.com/__rodrigkvii/" target="_blank">
-                                    <img src={Instagram} alt="instagram" /> <span>@__rodrigkvii</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="mailto:rodrigokvk@gmail.com" target="_blank">
-                                    <img src={Email} alt="gmail" /> <span>rodrigokvk@gmail.com</span>
-                                </a>
-                            </li>
+                            
                         </ul>
                     </div>
                 </div>

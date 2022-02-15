30f3c3d81c9d6cad6c222539fc2af6a3d6eddfc8
Rodrigo Kovalski <rodrigokvk@gmail.com>
2022-02-15 19:32:20 -0300

    update

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,11 @@ import styles from './styles.module.scss'
 import { Link , animateScroll as scroll } from 'react-scroll'
 
 import ArrowUp from '../../assets/chevron-up.svg'
-
+import MyPic from '../../assets/profile.png'
+import Insta from '../../assets/insta.svg'
+import Linkedin from '../../assets/linkedin.svg'
+import Git from '../../assets/github.svg'
+import Email from '../../assets/email.svg'
 
 export function Footer(){
     return (
@@ -11,10 +15,30 @@ export function Footer(){
             <div className={styles.container}>
                 <div className={styles.contact}>
                     <div className={styles.picture}>
+                        <img src={MyPic} alt="Rodrigo Kovaslski" />
                     </div>
                     <div className={styles.info}>
                         <ul>
-                            
+                            <li>
+                                <a href="https://www.linkedin.com/in/rodrigokvk/" target="_blank">
+                                    <img src={Linkedin} alt="linkedin" /> <span>Linkedin/rodrigokvk</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/rodrigokovalski" target="_blank">
+                                    <img src={Git} alt="github" /> <span>GitHub/rodrigokovalski</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.instagram.com/__rodrigkvii/" target="_blank">
+                                    <img src={Insta} alt="instagram" /> <span>@__rodrigkvii</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="mailto:rodrigokvk@gmail.com" target="_blank">
+                                    <img src={Email} alt="gmail" /> <span>rodrigokvk@gmail.com</span>
+                                </a>
+                            </li>
                         </ul>
                     </div>
                 </div>
